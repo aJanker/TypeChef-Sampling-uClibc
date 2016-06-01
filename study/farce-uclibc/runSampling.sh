29e0e0ba7cd69e22f35ec64ad6120764c19fcf42
Andreas Janker <an.janker@googlemail.com>
2016-06-01 12:52:20 +0200

    single run scripts

---
+++ study/farce-uclibc/runSampling.sh
@@ -0,0 +1,138 @@
+#!/bin/bash -e
+#!/bin/bash -vxe
+
+
+filesToProcess() {
+  local listFile=filelist
+  cat $listFile
+  #awk -F: '$1 ~ /.c$/ {print gensub(/\.c$/, "", "", $1)}' < linux_2.6.33.3_pcs.txt
+}
+
+
+flags=""
+srcPath=$PWD/uClibc
+target=x86_64
+
+#--openFeat $srcPath/openFeaturesList.txt
+export partialPreprocFlags="--bdd --openFeat $srcPath/../openFeaturesList.txt --featureModelDimacs $srcPath/../uclibc.dimacs  --include header.h --include builtin.h --include $srcPath/include/libc-symbols.h  -I $srcPath -I $srcPath/include -I $srcPath/include/sys -I /usr/include/ -I /usr/lib/gcc/x86_64-linux-gnu/4.8.4/include-fixed -I /usr/lib/gcc/x86_64-linux-gnu/4.8.4/include  --reuseAST --recordTiming --parserstatistics  --openFeat=openFeaturesList.txt --adjustLines"
+
+flags() {
+  	name="$1"
+  	base="$(basename "$1")"
+
+
+	extraFlag="-I $srcPath/libc/sysdeps/linux -I $srcPath/libc/sysdeps/linux/common -I $srcPath/libc/sysdeps/linux/common/bits"	
+	#define __SSP__ in general then undefine it for specific files
+	extraFlag="$extraFlag -I $srcPath/libc/sysdeps/linux/$target -I $srcPath/libc/sysdeps/linux/$target/bits -D__SSP__"
+	
+	if grep -q "extra/locale/" <<< "$name"; then
+		extraFlag="$extraFlag -D__UCLIBC_GEN_LOCALE"
+	elif grep -q "libc/" <<< "$name"; then		
+		extraFlag="$extraFlag -I $srcPath/libpthread -I $srcPath/ldso/include -I $srcPath/ldso/ldso/$target" 		
+		
+		if grep -q "libc/inet/" <<< "$name"; then		
+			extraFlag="$extraFlag -DRESOLVER=\"resolv.c\""
+		fi
+
+		if grep -q "libc/misc/internals/__uClibc_main" <<< "$name"; then		
+			extraFlag="$extraFlag -U__SSP__"
+		fi	
+
+
+	elif grep -q "librt/" <<< "$name"; then	
+		extraFlag="$extraFlag  -DNOT_IN_libc -DIS_IN_librt -I $srcPath/ldso/include -I $srcPath/libpthread"	
+		#extraFlag="$extraFlag  -I $srcPath/ldso/ldso/$target -I $srcPath/ldso/include -I $srcPath/libpthread/nptl/sysdeps/$target -I $srcPath/libpthread/nptl -I $srcPath/libpthread/nptl/sysdeps/pthread -I $srcPath/libpthread/nptl/sysdeps/unix/sysv/linux/$target -I $srcPath/libpthread/nptl/sysdeps/unix/sysv/linux -DNOT_IN_libc -DIS_IN_librt"
+	elif grep -q "libintl/" <<< "$name"; then
+		extraFlag="$extraFlag  -DNOT_IN_libc -DIS_IN_libintl"
+	elif grep -q "libm/" <<< "$name"; then
+		extraFlag="$extraFlag -DNOT_IN_libc -DIS_IN_libm -D_IEEE_LIBM"
+	elif grep -q "libcrypt/" <<< "$name"; then
+		extraFlag="$extraFlag -DNOT_IN_libc -DIS_IN_libcrypt"
+	elif grep -q "libresolv/" <<< "$name"; then
+		extraFlag="$extraFlag -DNOT_IN_libc -DIS_IN_libresolv"
+	elif grep -q "libutil/" <<< "$name"; then
+		extraFlag="$extraFlag -DNOT_IN_libc -DIS_IN_libutil"
+	elif grep -q "ldso/ldso/" <<< "$name"; then
+		extraFlag="$extraFlag -DNOT_IN_libc -DIS_IN_rtld -I $srcPath/ldso/include -I $srcPath/ldso/ldso/$target"
+	elif grep -q "ldso/libdl/" <<< "$name"; then
+		 extraFlag="$extraFlag -DNOT_IN_libc -DIS_IN_libdl"
+	elif grep -q "libnsl/" <<< "$name"; then
+		extraFlag="$extraFlag -DNOT_IN_libc -DIS_IN_libnsl"
+	elif grep -q "libubacktrace/" <<< "$name"; then		
+		extraFlag="$extraFlag -I $srcPath/libpthread -I $srcPath/ldso/include -I $srcPath/librt -DNOT_IN_libc -DIS_IN_libubacktrace"
+	elif grep -q "utils/" <<< "$name"; then
+		extraFlag="$extraFlag -I $srcPath/ldso/include -I $srcPath/libc/misc/wchar --include $srcPath/include/elf.h -DL_iconv_main"
+	elif grep -q "libpthread/" <<< "$name"; then
+		extraFlag="$extraFlag -I $srcPath/libpthread -DIS_IN_libpthread -DNOT_IN_libc "
+
+		if grep -q "libpthread/nptl" <<< "$name"; then
+			
+			extraFlag="$extraFlag -I $srcPath/ldso/include -I $srcPath/ldso/ldso/$target -I $srcPath/libc/stdlib -I $srcPath/libpthread/nptl -I $srcPath/libpthread/nptl/sysdeps/pthread -I $srcPath/libpthread/nptl/sysdeps/$target -I $srcPath/libpthread/nptl/sysdeps/unix/sysv/linux -I $srcPath/libpthread/nptl/sysdeps/unix/sysv/linux/$target -D_GNU_SOURCE" 
+
+			if grep -q "libpthread/nptl/sysdeps/unix/sysv/linux/" <<< "$name"; then
+				extraFlag="$extraFlag -I $srcPath/librt"
+	
+				if grep -q "pt-pread_pwrite.c" <<< "$name"; then
+					extraFlag="$extraFlag -I $srcPath/libc/sysdeps/linux/$target -I $srcPath/libc/sysdeps/linux/common"
+				elif grep -q "mq_notify.c" <<< "$name"; then
+					extraFlag="$extraFlag -DIS_IN_librt"
+				elif grep -q "timer_create.c" <<< "$name"; then
+					extraFlag="$extraFlag -DIS_IN_librt"
+				elif grep -q "timer_delete.c" <<< "$name"; then
+					extraFlag="$extraFlag -DIS_IN_librt"
+				elif grep -q "timer_getoverr.c" <<< "$name"; then
+					extraFlag="$extraFlag -DIS_IN_librt"
+				elif grep -q "timer_gettime.c" <<< "$name"; then
+					extraFlag="$extraFlag -DIS_IN_librt"
+				elif grep -q "timer_routines.c" <<< "$name"; then
+					extraFlag="$extraFlag -DIS_IN_librt"
+				elif grep -q "timer_settime.c" <<< "$name"; then
+					extraFlag="$extraFlag -DIS_IN_librt"			
+				elif grep -q "librt-cancellation.c" <<< "$name"; then
+					extraFlag="$extraFlag -DIS_IN_librt"
+				elif grep -q "rt-unwind-resume.c" <<< "$name"; then
+					extraFlag="$extraFlag -DIS_IN_librt"
+				fi
+		
+			fi
+			if grep -q "libpthread/nptl_db" <<< "$name"; then								
+				extraFlag="$extraFlag -DIS_IN_libthread_db=1 "
+			fi
+		elif grep -q "libpthread/linuxthreads.old" <<< "$name"; then
+			dummy="dummy"
+			#extraFlag="$extraFlag -I $srcPath/libpthread/linuxthreads.old -I $srcPath/libpthread/linuxthreads.old/sysdeps/pthread -I $srcPath/libpthread/linuxthreads.old/sysdeps/$target -I $srcPath/libpthread/nptl/sysdeps/unix/sysv/linux/$target -I $srcPath/libpthread/linuxthreads.old/sysdeps/sparc -I $srcPath/libc/sysdeps/linux/$target -I $srcPath/librt"
+					
+
+		elif grep -q "libpthread/linuxthreads" <<< "$name"; then
+				
+			extraFlag="$extraFlag -I $srcPath/libpthread/linuxthreads -I $srcPath/libpthread/linuxthreads/sysdeps/pthread -I $srcPath/libpthread/linuxthreads/sysdeps/$target -I $srcPath/libc/sysdeps/linux/$target -I $srcPath/libpthread/linuxthreads/sysdeps/unix/sysv/linux -I $srcPath/libpthread/linuxthreads/sysdeps/unix/sysv/linux/$target -I $srcPath/librt"
+
+		fi
+		
+
+	fi
+
+	
+		
+	echo "$extraFlag"	 
+}
+
+#cat filelist | parallel -j 30 ./jcpp.sh $srcPath/{}.c $extraFlags
+
+filesToProcess|while read i; do
+## Reset output
+outDbg="$srcPath/$i.dbg"
+outErr="$srcPath/$i.err"
+extraFlags="$(flags "$i")"
+echo " $srcPath/$i.c $partialPreprocFlags $extraFlags"
+./sampling.sh $srcPath/$i.c $partialPreprocFlags $extraFlags 2> $outErr |tee $outDbg
+
+cat $outErr 1>&2
+
+ gzip -c ${outErr} > ${outErr}.gz
+ gzip -c ${outDbg} > ${outDbg}.gz
+
+ rm ${outErr}
+ rm ${outDbg}
+
+done
